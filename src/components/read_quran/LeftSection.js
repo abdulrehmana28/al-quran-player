610b137a3a3278c87130457e7b6165efb3ad2fee
Abdulrehman Azam <abdulrehmana28@skiff.com>
2024-03-31 12:44:30 +0500

    added search component
    some problem with dependencies now

--- src/components/read_quran/LeftSection.js
+++ src/components/read_quran/LeftSection.js
@@ -1,7 +1,32 @@
 function LeftSection() {
-    return <div className="bg-slate-400 basis-1/4">
+  return (
+    <div className="bg-slate-300 basis-1/4 flex flex-col">
+      {/* TextField Search for Surah*/}
+      <InputSearch />
+
+      {/* List name surah */}
+
+        <ListSurah/>
 
     </div>
+  );
 }
 
 export default LeftSection;
+
+function InputSearch() {
+  return (
+    <div className="bg-gray-500 w-full h-24 flex justify-center items-center">
+      <div className="bg-white w-full  mx-4 rounded-xl py-1 px-8 relative">
+        <img
+          src="search.png"
+          alt="Search icon"
+          width={20}
+          height={20}
+          className="absolute top-3 left-4"
+        />
+        <input className="h-full w-full py-2 ml-4" placeholder="Search Surah" />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/read_quran/ListSurah.js
@@ -0,0 +1,7 @@
+function ListSurah() {
+    return <div className="h-full w-full p-4">
+    list surah
+</div>
+}
+
+export default ListSurah
